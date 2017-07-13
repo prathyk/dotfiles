414594044a89b9a10a5a15d731873531dd0ac015
Prathyk <prathyk@gmail.com>
2017-07-13 13:45:17 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+stow -t ~ bin
+stow -t ~ git
+stow -t ~ global
+stow -t ~ local
+stow -t ~ powershell
+stow -t ~ profile
+stow -t /usr/bin runcached
+stow -t ~ shell
+stow -t ~ spacevim
+stow -t ~ tmux
+stow -t ~ vim
+stow -t ~ vimperator
+stow -t ~ zsh
