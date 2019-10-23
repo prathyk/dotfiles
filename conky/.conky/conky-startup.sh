33b632c8be09425ee4b73dd1d636dfbed2649813
Pratik <prathyk@gmail.com>
2019-10-23 22:43:00 +0530

    fresh on manjaro

--- conky/.conky/conky-startup.sh
+++
@@ -1,6 +0,0 @@
-sleep 20s
-killall conky
-cd "/home/osboxes/.conky/Green Apple Desktop"
-conky -c "/home/osboxes/.conky/Green Apple Desktop/Gotham" &
-cd "/home/osboxes/.conky/Green Apple Desktop"
-conky -c "/home/osboxes/.conky/Green Apple Desktop/conky_seamod" &
